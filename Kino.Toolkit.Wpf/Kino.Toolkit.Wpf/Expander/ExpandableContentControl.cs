8923cd1bd5883fb250c4763ce31fb6d43ad91220
Dino Chan <dino.chan@live.com>
2019-04-18 14:11:30 +0800

    完善ExpanderSample

--- Kino.Toolkit.Wpf/Kino.Toolkit.Wpf/Expander/ExpandableContentControl.cs
+++ Kino.Toolkit.Wpf/Kino.Toolkit.Wpf/Expander/ExpandableContentControl.cs
@@ -12,32 +12,38 @@ namespace Kino.Toolkit.Wpf.Primitives
     public class ExpandableContentControl : ContentControl
     {
         /// <summary>
-        /// 标识 IsExpand 依赖属性。
+        /// 标识 Pentage 依赖属性。
         /// </summary>
-        public static readonly DependencyProperty IsExpandProperty =
-            DependencyProperty.Register(nameof(IsExpand), typeof(bool), typeof(ExpandableContentControl), new FrameworkPropertyMetadata(default(bool), FrameworkPropertyMetadataOptions.AffectsMeasure));
+        public static readonly DependencyProperty PercentageProperty =
+            DependencyProperty.Register(nameof(Percentage), typeof(double), typeof(ExpandableContentControl), new FrameworkPropertyMetadata(1d, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        static ExpandableContentControl()
+        {
+
+        }
 
         /// <summary>
-        /// 获取或设置IsExpand的值
+        /// 获取或设置Pentage的值
         /// </summary>
-        public bool IsExpand
+        public double Percentage
         {
-            get => (bool)GetValue(IsExpandProperty);
-            set => SetValue(IsExpandProperty, value);
+            get => (double)GetValue(PercentageProperty);
+            set => SetValue(PercentageProperty, value);
         }
 
         protected override Size MeasureOverride(Size constraint)
         {
-            if (IsExpand)
-                return base.MeasureOverride(constraint);
-
             int count = VisualChildrenCount;
             Size childConstraint = new Size(Double.PositiveInfinity, Double.PositiveInfinity);
             UIElement child = (count > 0) ? GetVisualChild(0) as UIElement : null;
+            var result = new Size();
             if (child != null)
+            {
                 child.Measure(childConstraint);
+                result = child.DesiredSize;
+            }
 
-            return default;
+            return new Size(result.Width * Percentage, result.Height * Percentage);
         }
 
         protected override Size ArrangeOverride(Size arrangeBounds)
@@ -46,10 +52,10 @@ namespace Kino.Toolkit.Wpf.Primitives
             UIElement child = (count > 0) ? GetVisualChild(0) as UIElement : null;
             if (child != null)
             {
-                var childArrangeBounds = new Rect(arrangeBounds);
-                arrangeBounds.Width = Math.Max(arrangeBounds.Width, child.DesiredSize.Width);
-                arrangeBounds.Height = Math.Max(arrangeBounds.Height, child.DesiredSize.Height);
-                child.Arrange(new Rect(new Point(0, 0), arrangeBounds));
+                Size childArrangeBounds = arrangeBounds;
+                childArrangeBounds.Width = Math.Max(arrangeBounds.Width, child.DesiredSize.Width);
+                childArrangeBounds.Height = Math.Max(arrangeBounds.Height, child.DesiredSize.Height);
+                child.Arrange(new Rect(new Point(0, 0), childArrangeBounds));
             }
 
             return arrangeBounds;
