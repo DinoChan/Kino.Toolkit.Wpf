8acba02e297692006d524146b42a19725b50214c
Dino Chan <dino.chan@live.com>
2019-04-19 22:28:15 +0800

    增加IsMultiSelectCheckBoxEnabled

--- Kino.Toolkit.Wpf/Kino.Toolkit.Wpf/ListBox/KinoListBox.cs
+++ Kino.Toolkit.Wpf/Kino.Toolkit.Wpf/ListBox/KinoListBox.cs
@@ -10,6 +10,16 @@ namespace Kino.Toolkit.Wpf
 {
     public class KinoListBox : ListBox
     {
+        // Using a DependencyProperty as the backing store for IsMultiSelectCheckBoxEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsMultiSelectCheckBoxEnabledProperty =
+            DependencyProperty.Register(nameof(IsMultiSelectCheckBoxEnabled), typeof(bool), typeof(KinoListBox), new PropertyMetadata(true));
+
+        public bool IsMultiSelectCheckBoxEnabled
+        {
+            get { return (bool)GetValue(IsMultiSelectCheckBoxEnabledProperty); }
+            set { SetValue(IsMultiSelectCheckBoxEnabledProperty, value); }
+        }
+
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new KinoListBoxItem();
